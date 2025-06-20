bb1c685552c8b0f5234db69af023ce623bd96104
dimitrisjasmy <dimitris@jasmylab.com>
2025-06-20 15:21:30 +0300

    removed history

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,19 +6,19 @@ import { setupLayouts } from "virtual:generated-layouts";
 import routes from "~pages";
 
 const router = createRouter({
-  history: createWebHistory('JANCTION'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [...setupLayouts(routes)],
 });
 
 //update current blockchain
 router.beforeEach((to) => {
   const { chain } = to.params
-    if(chain){
-    const blockchain= useBlockchain()
-  if (chain !== blockchain.chainName) {
-    blockchain.setCurrent(chain.toString())
+  if (chain) {
+    const blockchain = useBlockchain()
+    if (chain !== blockchain.chainName) {
+      blockchain.setCurrent(chain.toString())
+    }
   }
-    } 
 })
 
 // Docs: https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
